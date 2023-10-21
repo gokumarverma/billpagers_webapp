9107c83b8bb995566c743da3d81ad1199d4eab5d
Anil Kumar <go_kumaranil@yahoo.com>
2023-10-21 06:31:58 +0530

    added basic control flow

--- app.py
+++ app.py
@@ -18,6 +18,13 @@ def puppy_latin(name):
     else:
         l_name=name+"y"
     return '<h1>hi {}!! Your latin name is {}</h1>'.format(name,l_name)
+
+@app.route("/puppies")
+def puppies():
+    name=["rufus","goku","fluffy","tommy"]
+
+    return render_template("index.html",name=name)
+
     
 if __name__ == "__main__":
     app.run(debug=True)
